af5b74358d4610907f54c836226d37ec03ce0a90
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-21 04:47:19 +0900

    refactor: strengthen kernel boundaries and contracts

--- src/architecture/index.ts
+++ src/architecture/index.ts
@@ -9,3 +9,4 @@ export * from "./change-gates.js";
 export * from "./budget.js";
 export * from "./repair.js";
 export * from "./guard.js";
+\nexport * from "./runtime.js";\nexport * from "./cancellation.js";\nexport * from "./retry.js";\nexport * from "./circuit-breaker.js";\n

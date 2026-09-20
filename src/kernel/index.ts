ed96ff30f451a2399a314febd7db0d76a2e4adc0
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-21 04:15:42 +0900

    feat: export durable outbox dispatcher

--- src/kernel/index.ts
+++ src/kernel/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./database.js";
 export * from "./sqlite-store.js";
 export * from "./scheduler.js";
+export * from "./outbox-dispatcher.js";
 export * from "./kernel.js";

0b1122facf71e775b7d9e6b022bdab09e4708f90
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-21 04:47:23 +0900

    refactor: strengthen kernel boundaries and contracts

--- src/kernel/index.ts
+++ src/kernel/index.ts
@@ -4,3 +4,4 @@ export * from "./sqlite-store.js";
 export * from "./scheduler.js";
 export * from "./outbox-dispatcher.js";
 export * from "./kernel.js";
+\nexport * from "./ports.js";\nexport * from "./step-state.js";\n

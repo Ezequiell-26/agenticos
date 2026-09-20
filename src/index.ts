757fa928d1afa4b9ef026f20d6f90e08826c9b89
KinetixFitt <m.ezequiel.gonzalez25@gmail.com>
2026-09-21 04:13:53 +0900

    feat: expose durable kernel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from "./architecture/index.js";
+export * from "./kernel/index.js";
 export * from "./providers/types.js";
 export * from "./providers/catalog.js";
 export * from "./providers/openai-compatible.js";
